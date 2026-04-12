3ba8af4d5273da4e38393c4a2dfbfaffcbbb6a28
EmericSauthier <Emeric.Sauthier@etu.univ-smb.fr>
2026-04-12 21:31:58 +0200

    test failed

--- server/__tests__/index.js
+++ server/__tests__/index.js
@@ -3,6 +3,6 @@ import { describe, expect, test } from "@jest/globals";
 
 describe("distance", () => {
     test("(50;50) -> (-10;-10) --> should ~ 8 831,87 km", () => {
-        expect(getDistance({ lng: 50, lat: 50 }, { lng: -10, lat: -10 })).toBeCloseTo(8831.87)
+        expect(getDistance({ lng: 50, lat: 50 }, { lng: -10, lat: -10 })).toBeCloseTo(0)
     })
 })
